066dacdb5900896ee0ce54e054c2818c4a044094
Jessy Hu <jesshuy12@gmail.com>
2019-02-05 06:42:12 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(*arguments)            # invoking the splat operator
    array = []                        #example1 - return array
-   self.each do |key, value|          #iterating using the self because we need to iterate thru the hash of this lab, and using "self" allows
+   self.each do |key, value|          #iterating using the self because we need to iterate thru the                                     hash of this lab, and using "self" points to the object that                                      "owns" the currently executing code
     if arguments.include?(value)      # determine if values is included in arguements
       array << key                   #if the if statement is true, push key into array
     end
