1fb5dc55b7909f2e8476fb9ad39c56a11ed342ef
Jessy Hu <jesshuy12@gmail.com>
2019-02-05 06:36:31 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,8 @@ class Hash
   def keys_of(*arguments)            # invoking the splat operator
    array = []                        #example1 - return array
    self.each do |key, value|
-    if arguments.include?(value)
-      array << key
+    if arguments.include?(value)      # determine if values is included in 
+      array << key                   #if the if statement is true, push key into array
     end
   end
   return array
